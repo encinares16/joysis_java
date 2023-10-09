6c00e66ceaf09b7a898ba2bf444c2df8e5bb864b
encinares16 <rmncnrs16@gmail.com>
2023-10-09 13:58:53 +0800

    commit 10/09

---
+++ src/java_day6/operators/Activity3_Relational_Operator.java
@@ -0,0 +1,48 @@
+package java_day6.operators;
+
+public class Activity3_Relational_Operator {
+    
+    static byte a, b ,c;
+    public static void main(String[] args) {
+       
+        a = 10;
+        b = 12;
+        c = 15;
+
+        System.out.println("Relational Operator  <,>, <=, >=, == ,!= \n");
+        
+        System.out.println("Given Values");
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+        System.out.println("C = " + c);
+
+        System.out.println("\nLess Than [<]");
+        System.out.println( "Is " + a + " is less than to " + b + " ? " + (a < b));
+        System.out.println( "Is " + c + " is less than to " + b + " ? " + (c < b));
+        
+        System.out.println("\nGreater Than [>]");
+        System.out.println( "Is " + a + " is greater than to " + b + " ? " + (a > b));
+        System.out.println( "Is " + c + " is greater than to " + b + " ? " + (c > b));
+        
+        System.out.println("\nLess than Equal [<=]");
+        System.out.println( "Is " + a + " is less than equal to " + b + " ? " + (a <= b));
+        System.out.println( "Is " + a + " is less than equal to " + a + " ? " + (a <= a));
+        System.out.println( "Is " + c + " is less than equal to " + a + " ? " + (c <= a));
+        
+        System.out.println("\nGreater than Equal [>=]");
+        System.out.println( "Is " + a + " is greater than equal to " + b + " ? " + (a >= b));
+        System.out.println( "Is " + a + " is greater than equal to " + a + " ? " + (a >= a));
+        System.out.println( "Is " + c + " is greater than equal to " + a + " ? " + (c >= a));
+                
+        System.out.println("\nEqual To [==]");
+        System.out.println( "Is " + a + " is equal to " + b + ": " + (a == b));
+        System.out.println( "Is " + a + " is equal to " + a + ": " + (a == a));
+        System.out.println( "Is " + c + " is equal to " + a + ": " + (c == a));
+                        
+        System.out.println("\nNot Equal To [!=]");
+        System.out.println( "Is " + a + " is not equal to " + b + ": " + (a != b));
+        System.out.println( "Is " + a + " is not equal to " + a + ": " + (a != a));
+        System.out.println( "Is " + c + " is not equal to " + a + ": " + (c != a));
+        
+    }
+}
