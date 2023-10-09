1304bec825e48d2cb2293d61fb26eba74957e532
encinares16 <rmncnrs16@gmail.com>
2023-10-09 14:40:15 +0800

    commit 10/09

---
+++ src/java_day6/operators/Activity3_OR_Operator.java
@@ -0,0 +1,21 @@
+package java_day6.operators;
+
+public class Activity3_OR_Operator {
+
+    static boolean output;
+    static byte inputA, inputB;
+    public static void main(String[] args) {
+        
+        inputA = 0; 
+        inputB = 1;
+        
+        System.out.println("OR Operator [&&]");
+        System.out.println("[0] False, [1] True");
+        
+        System.out.println("[OR] Truth Table"); // or Logic gates
+        System.out.println("Input A: " + inputA + " | Input B: " + inputA + " | Output: " + (output = false || false));
+        System.out.println("Input A: " + inputA + " | Input B: " + inputB + " | Output: " + (output = false || true));
+        System.out.println("Input A: " + inputB + " | Input B: " + inputA + " | Output: " + (output = true || false));
+        System.out.println("Input A: " + inputB + " | Input B: " + inputB + " | Output: " + (output = true || true));
+    }
+}
---
+++ src/java_day6/operators/Activity3_Ternary_Operator.java
@@ -0,0 +1,23 @@
+package java_day6.operators;
+
+public class Activity3_Ternary_Operator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ternary Operator"); // if else short hand
+        boolean isTodayMonday = true;
+        
+        System.out.println("[Regular If Else]");
+        
+        if(!isTodayMonday){
+            System.out.println("Yes, Today is Monday");
+        } else {
+            System.out.println("Boolean value if " + !isTodayMonday);
+            System.out.println("No, Today is other day\n");
+        }
+        
+        System.out.println("[Ternary]");
+        System.out.println("Boolean value: " + isTodayMonday);
+        System.out.println((isTodayMonday) ? "Yes, Today is Monday" : "No, Today is Sunday");
+    }
+}
