613bd500dd51363a296fbb9099280b3d2e6fb967
encinares16 <rmncnrs16@gmail.com>
2023-10-11 12:13:29 +0800

    commit 10/11

--- src/java_day7/controlflow/IfElseStatement.java
+++ src/java_day7/controlflow/IfElseStatement.java
@@ -1,13 +1,25 @@
 package java_day7.controlflow;
 
+import java.util.Scanner;
+
 public class IfElseStatement {
     public static void main(String[] args) {
-        String day = "Sunday";
+//        Scanner scanner = new Scanner(System.in);
+//        String day = scanner.nextLine();
+        String name = "Jerome";
+//        System.out.println("Enter day");
         
-        if(day == "Saturday" || day == "Sunday"){
-            System.out.println("It's Weekend");
-        } else {
-            System.out.println("It's Weekday");
-        }
+        
+        String x = "Java";
+   
+//        System.out.println(day.equals(name));
+//        if(day.equals(day) == day.equals(day)){
+//            System.out.println("It's Weekend");
+//        } else {
+//            System.out.println("It's Weekday");
+//        }
+
+        //shorthand
+
     }
 }
