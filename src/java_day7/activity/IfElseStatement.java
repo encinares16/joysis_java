5de36e3b7ae455a4bd64ef795d4d3bb90f4d3b0c
encinares16 <rmncnrs16@gmail.com>
2023-10-12 09:59:16 +0800

    commit 10/12

---
+++ src/java_day7/activity/IfElseStatement.java
@@ -0,0 +1,25 @@
+package java_day7.activity;
+
+import java.util.Scanner;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String day = scanner.nextLine();
+        String name = "Jerome";
+//        System.out.println("Enter day");
+        
+        
+        String x = "Java";
+   
+//        System.out.println(day.equals(name));
+//        if(day.equals(day) == day.equals(day)){
+//            System.out.println("It's Weekend");
+//        } else {
+//            System.out.println("It's Weekday");
+//        }
+
+        //shorthand
+
+    }
+}
