9aeefb847220208af9b9823390cbd74be5e522df
encinares16 <rmncnrs16@gmail.com>
2023-10-19 12:02:49 +0800

    update day15 dir 10/19

--- src/day15/methods/Methods.java
+++ src/day15/methods/Methods.java
@@ -4,13 +4,19 @@ public class Methods {
     public static void main(String[] args) {
         displayName("Summer");
         getProduct(5, 5, 5);
-        getProduct(5, 10, 15);
+        getProduct(5, 10, 5);
+        
+        System.out.println(Methods.sum(1, 2));
     }
     
     static void displayName(String name){
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(name);
         }
+    }
+    
+    public static int sum(int a, int b){ 
+        return a + b;
     }
     
     static void getProduct(int x, int y, int z){
