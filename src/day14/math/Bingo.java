962cd248bfbd33b67be4256b885e397fee4da0ed
encinares16 <rmncnrs16@gmail.com>
2023-10-20 06:39:13 +0800

    commit 10/20

--- src/day14/math/Bingo.java
+++ src/day14/math/Bingo.java
@@ -1,5 +1,7 @@
 package day14.math;
 
+import java.util.Scanner;
+
 public class Bingo {
     public static void main(String[] args) {
         
@@ -60,6 +62,99 @@ public class Bingo {
 
                             // card[0][0][0][3][0] = 9;   
                             if (card[0][0][0][l][0] <= 9) {
+                                System.out.print(" " + card[i][j][k][l][m]);
+                            } else {
+                                System.out.print(card[i][j][k][l][m] + " ");
+                            }
+                        }
+                        System.out.println(" ");
+                    }
+                }
+            }
+        }
+        
+        Scanner scan = new Scanner(System.in);
+        int gen = 0;
+        do {
+            System.out.print("\nType 'G' to generate: ");
+            String yes = scan.nextLine().toLowerCase();
+            if (yes.equals("g")) {
+                generate();
+                gen = 1;
+            } else {
+                System.out.println("Program Terminated");
+                System.exit(0);
+            }
+        } while (gen == 1);
+    }
+    
+    public static void delay(int milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+             Thread.currentThread().isInterrupted();
+        }
+    }
+    
+    // throws InterruptedException
+    static void generate() {
+        int card[][][][][] = new int[1][1][1][5][5];
+        
+        System.out.println("");
+        for (int i = 0; i < 5 ; i++) {
+            
+            // row B
+            card[0][0][0][i][0] = (int) (Math.random() * 15) + 1;
+            for (int j = 0; j < i; j++) {
+                if (card[0][0][0][i][0] == card[0][0][0][j][0]) {
+                    i--;
+                }
+            }   
+                                // System.out.println((30 - 16 + 1) + 16); -> 31
+            // row I         if Math random generate 30 - 16 = (14 + 1) + 16 (max - min) + max
+            card[0][0][0][i][1] = (int) (Math.random()*(30 - 16 + 1) + 16);
+            for (int j = 0; j < i; j++) {
+                if (card[0][0][0][i][0] == card[0][0][0][j][0]) {
+                    i--;
+                }
+            }
+            
+            // row N
+            card[0][0][0][i][2] = (int) (Math.random()*(45 - 31 + 1) + 31);
+            for (int j = 0; j < i; j++) {
+                if (card[0][0][0][i][2] == card[0][0][0][j][2]) {
+                    i--;
+                } 
+                card[0][0][0][2][2] = 99;
+            }
+            
+            // row G
+            card[0][0][0][i][3] = (int) (Math.random()*(60 - 46 + 1) + 46);
+            for (int j = 0; j < i; j++) {
+                if (card[0][0][0][i][3] == card[0][0][0][j][3]) {
+                    i--;
+                } 
+            }
+            
+            // row O
+            card[0][0][0][i][4] = (int) (Math.random()*(75 - 61 + 1) + 61);
+            for (int j = 0; j < i; j++) {
+                if (card[0][0][0][i][4] == card[0][0][0][j][4]) {
+                    i--;
+                } 
+            }
+        }
+        
+        System.out.println("B  I  N  G  O");
+        // iterate, traverse card array
+        for (int i = 0; i < card.length; i++) {
+            for (int j = 0; j < card[i].length; j++) {
+                for (int k = 0; k < card[i][j].length; k++) {
+                    for (int l = 0; l < card[i][j][k].length; l++) {
+                        for (int m = 0; m < card[i][j][k][l].length; m++) {
+                            delay(600);
+                            if (card[0][0][0][l][0] < 10) {
+                                // format b row spacing if < 10, add space before the number
                                 System.out.print(" " + card[i][j][k][l][m]);
                             } else {
                                 System.out.print(card[i][j][k][l][m] + " ");
