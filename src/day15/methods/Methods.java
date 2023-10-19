c469d8183932a01f85b3a610d11a8fbbad20dafd
encinares16 <rmncnrs16@gmail.com>
2023-10-19 11:46:13 +0800

    create day15 dir 10/19

---
+++ src/day15/methods/Methods.java
@@ -0,0 +1,14 @@
+package day15.methods;
+
+public class Methods {
+    public static void main(String[] args) {
+        displayName("Summer");
+    }
+    
+    static void displayName(String name){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name);
+        }
+    }
+}
+
