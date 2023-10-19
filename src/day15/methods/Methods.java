406d084307acd36ea9e07b7d1c816b0b2c62396b
encinares16 <rmncnrs16@gmail.com>
2023-10-19 11:52:17 +0800

    update day15 dir 10/19

--- src/day15/methods/Methods.java
+++ src/day15/methods/Methods.java
@@ -3,12 +3,18 @@ package day15.methods;
 public class Methods {
     public static void main(String[] args) {
         displayName("Summer");
+        getProduct(5, 5, 5);
+        getProduct(5, 10, 15);
     }
     
     static void displayName(String name){
         for (int i = 0; i < 10; i++) {
             System.out.println(name);
         }
+    }
+    
+    static void getProduct(int x, int y, int z){
+        System.out.println(x * y * z);
     }
 }
 
